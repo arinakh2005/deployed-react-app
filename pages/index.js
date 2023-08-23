19d6a3a01de215a20663ae3798ab918362e91ffd
arinakh2005 <arinakholodnytska3@gmail.com>
2023-08-23 17:24:10 +0300

    feat: add connection with MongoDB

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,22 @@
+import { MongoClient } from 'mongodb';
 import MeetupList from '../components/meetups/MeetupList';
 
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A First Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 5, 12345 Some City',
-    description: 'This is a first meetup!'
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 10, 12345 Some City',
-    description: 'This is a second meetup!'
-  }
-];
+// const DUMMY_MEETUPS = [
+//   {
+//     id: 'm1',
+//     title: 'A First Meetup',
+//     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+//     address: 'Some address 5, 12345 Some City',
+//     description: 'This is a first meetup!'
+//   },
+//   {
+//     id: 'm2',
+//     title: 'A Second Meetup',
+//     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+//     address: 'Some address 10, 12345 Some City',
+//     description: 'This is a second meetup!'
+//   }
+// ];
 
 export function HomePage(props) {
   // * Another way to solve problem (instead of getStaticProps [method name is IMPORTANT, NOT CHANGE IT]
@@ -42,9 +43,23 @@ export function HomePage(props) {
 // }
 
 export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    'mongodb+srv://test:test@cluster0.o0ytu3a.mongodb.net/meetups?retryWrites=true&w=majority',
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection('meetups');
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+        meetups: meetups.map((meetup) => ({
+          title: meetup.title,
+          address: meetup.address,
+          image: meetup.image,
+          id: meetup._id.toString(),
+      })),
     },
     revalidate: 1,
   };
